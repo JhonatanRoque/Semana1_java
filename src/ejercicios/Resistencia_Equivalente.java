53d1329ca268815e9566c2cc938ab00785f47169
JhonatanRoque <franciscoabarca7392@gmail.com>
2022-03-10 10:54:53 -0600

    Ejercicio1: Calcular Resistencia de Equivalencia

--- src/ejercicios/Resistencia_Equivalente.java
+++ src/ejercicios/Resistencia_Equivalente.java
@@ -1,21 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicios;
 
-/**
- *
- * @author ITCA
- */
-public class Resistencia_Equivalente {
+     import java.text.DecimalFormat;
+    import java.text.NumberFormat;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Resistencia_Equivalente {
+    /*
+     Se tienen 4 resistencias en paralelo R1 = 5.1 ohm, R2 = 68 ohm,
+    R3 = 75 ohm y R4 = 82 ohm; obtener la resistencia equivalente
+    
+    */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Declaraciòn de formato de decimal
+        NumberFormat formato = new DecimalFormat("#0.00");
+        //Declaraciòn de variable
+        double Rest1, Rest2, Rest3, Rest4, RestE;
+        
+        //Inicializaciòn de variables
+        Rest1 = 5.1;
+        Rest2 = 68;
+        Rest3 = 75;
+        Rest4 = 82;
+        
+        //uso de variable
+        RestE = 1 /((1/Rest1)+(1/Rest2)+(1/Rest3)+(1/Rest4));
+        
+        //Mostrar resultado
+        System.out.println("Resistencia 1:  " + Rest1 + " ohm");
+        System.out.println("Resistencia 2:  " + Rest2 + " ohm");
+        System.out.println("Resistencia 3:  " + Rest3 + " ohm");
+        System.out.println("Resistencia 4:  " + Rest4 + " ohm");
+        System.out.println("Resistencia Equivalente:  " + formato.format(RestE) + " ohm");
     }
     
 }
